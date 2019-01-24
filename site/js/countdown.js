ce1ddcb989e17c0fe2c4621bd695bf60625cda5e
Lucas Ekstener <lucas.ekstener@learnet.se>
2019-01-24 15:46:32 +0100

    A lot of code. Javascript and such

---
+++ site/js/countdown.js
@@ -0,0 +1,26 @@
+var countDownDate = new Date("Dec 24, 2019 23:59:59").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000*60*60*24));
+    var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+    var seconds = Math.floor((distance % (1000*60)) / 1000);
+    document.getElementById("timer").innerHTML = days + "d:" + hours + "h:" + minutes + "m:" + seconds +"s" ;
+
+
+    if (distance <= 0 ) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "<center>The Revolution Has Begun</center>";
+        var element1 = document.getElementById("main");
+        element1.parentNode.removeChild(element1);
+        var element2 = document.getElementById("titel");
+        element2.parentNode.removeChild(element2);
+        var element3 = document.getElementById("slogan");
+        element3.parentNode.removeChild(element3);
+        var element4 = document.getElementById("timertext");
+        element4.parentNode.removeChild(element4);
+         
+    }
+}, 1000)
+
